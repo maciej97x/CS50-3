77e40ed7516e0acd2903fc88e54263420cb62594
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-20 12:40:29 +0000

    Fri, Oct 20, 2023, 12:40 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,6 +19,15 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+
+    if (score1 > score2);
+    {
+        printf("Player 1 win! ");
+    }
+    else if (score1 < score2);
+    {
+        printf("Player 2 win! ");
+    }
 }
 
 int compute_score(string word)
