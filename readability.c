ed10752c560919bd70fe1ac86f532eac012df9e5
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-21 06:11:14 +0000

    Sat, Oct 21, 2023, 6:11 AM +00:00

--- readability.c
+++ readability.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(void)
 
@@ -13,6 +14,7 @@ int main(void)
 
     for (int i = 0; i < strlen(text); i++)
     {
-        if 
+        if (isalpha(text[i]))
+        letters++;
     }
 }
