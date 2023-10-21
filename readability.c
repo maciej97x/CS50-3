28a8ededd9d02dd06f07ecfa94617c7348a09715
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-21 06:09:28 +0000

    Sat, Oct 21, 2023, 6:09 AM +00:00

---
+++ readability.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+
+{
+    string text = get_string("Text ");
+
+    int letters = 0;
+    int words = 0;
+    int sentences = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if 
+    }
+}
