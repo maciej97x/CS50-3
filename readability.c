d50dd62b64daf03275d48630815192432a07e5bf
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-21 06:13:05 +0000

    Sat, Oct 21, 2023, 6:13 AM +00:00

--- readability.c
+++ readability.c
@@ -9,12 +9,16 @@ int main(void)
     string text = get_string("Text ");
 
     int letters = 0;
-    int words = 0;
+    int words = 1;
     int sentences = 0;
 
     for (int i = 0; i < strlen(text); i++)
     {
-        if (isalpha(text[i]))
+        if(isalpha(text[i]))
         letters++;
+    }
+    else if(text[i] == " ")
+    {
+        words++;
     }
 }
