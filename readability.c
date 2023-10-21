d5291c5fd5ce43fe552f0c29572ef3cc27e5ab2c
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-21 06:34:02 +0000

    Sat, Oct 21, 2023, 6:34 AM +00:00

--- readability.c
+++ readability.c
@@ -26,8 +26,8 @@ int main(void)
     {
         sentences++;
     }
-    float L = (float)letters / (float)words * 100
-    float S = (float)sentences / (float)words * 100
+    float L = (float)letters / (float)words * 100;
+    float S = (float)sentences / (float)words * 100;
     int index = round(0,0588 * L - 0,296 * S - 15,8);
 
     if(index < 1)
