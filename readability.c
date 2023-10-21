719bb08bc238c9adf6ec7800e7d6a2c62d0d44d5
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-21 06:32:23 +0000

    Sat, Oct 21, 2023, 6:32 AM +00:00

--- readability.c
+++ readability.c
@@ -22,7 +22,7 @@ int main(void)
     {
         words++;
     }
-    else if(text == "." || text == "!" || text == "?")
+    else if(text == '.' || text == '!' || text == '?')
     {
         sentences++;
     }
