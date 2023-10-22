df5badf91f6d3ade55822f8d79d002010b9adab8
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-22 12:14:01 +0000

    Sun, Oct 22, 2023, 12:14 PM +00:00

--- sort/plurality/smiley/helpers.c
+++ sort/plurality/smiley/helpers.c
@@ -3,9 +3,9 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
-    for (int = 0; row < width; row++)
+    for (int row = 0; row < width; row++)
     {
-        for (int column = 0; column < height; columng++)
+        for (int column = 0; column < height; column++)
         {
             if (image[column][row].rgbtBlue == 0 && image[column][row].rgbtGreen == 0 && image[column][row].rgbtRed == 0)
             {
