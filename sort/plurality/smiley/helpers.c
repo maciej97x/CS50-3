b789d2536a23e75307d3c897eb33d0bae1878113
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-22 12:11:20 +0000

    Sun, Oct 22, 2023, 12:11 PM +00:00

--- sort/plurality/smiley/helpers.c
+++ sort/plurality/smiley/helpers.c
@@ -8,6 +8,11 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
         for (int column = 0; column < height; columng++)
         {
             if (image[column][row].rgbtBlue == 0 && image[column][row].rgbtGreen == 0 && image[column][row].rgbtRed == 0)
+            {
+                image[column][row].rgbtBlue
+                image[column][row].rgbtRed
+                image[column][row].rgbtGreen
+            }
         }
     }
 
