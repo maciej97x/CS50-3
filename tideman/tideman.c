03cb2da52ad51786be3169f33a20c08f19fd78d4
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-23 09:30:01 +0000

    Mon, Oct 23, 2023, 9:30 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -100,6 +100,14 @@ int main(int argc, string argv[])
 bool vote(int rank, string name, int ranks[])
 {
     // TODO
+    for (int = 0; i < candidate_count; i++)
+    {
+        if(strcmp(name, candidates[i] == 0))
+        (
+            ranks[rank] = i;
+            return true;
+        )
+    }
     return false;
 }
 
