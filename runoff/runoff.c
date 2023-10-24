fad3c5865daf618335c12b67c3ea106241b09b6e
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-24 04:42:48 +0000

    Tue, Oct 24, 2023, 4:42 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -1,14 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <math.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
 #define MAX_CANDIDATES 9
 
-// preferences[i][j] is jth preference for voter i(2d array)
+// preferences[i][j] is jth preference for voter i
 int preferences[MAX_VOTERS][MAX_CANDIDATES];
 
 // Candidates have name, vote count, eliminated status
@@ -128,49 +125,16 @@ int main(int argc, string argv[])
 }
 
 // Record preference if vote is valid
-//passed i,j and name of the candidate the user entered, look at line 77
 bool vote(int voter, int rank, string name)
 {
-    bool exist = false;
-    for (int i = 0; i < candidate_count; i++)
-    {
-        //check if name is present in the candidates entered by the user by camparing two strings
-        //strcmp is checking for the name and camparing it to the candidates array location 'i' which starts according to the for loop above
-        if (strcmp(name, candidates[i].name) == 0)
-        {
-            //if you found the person is present then add that number as a rank of the candidate in the preferences array
-            //suppose this is a 2d array and the preference array is adding the preference number on a specific poistion so
-            // here ex - preferences[0][0] = i (the rank preferences from the candidate count)
-            // [i][][][]
-            // [][][][]
-            // [][][][]
-            // [][][][]
-            preferences[voter][rank] = i;
-            exist = true;
-            break;
-            //This is a bool conditions which will become true, again look at line 77
-        }
-    }
-    return exist;
+    // TODO
+    return false;
 }
 
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
     // TODO
-    for (int i = 0; i < voter_count; i++)
-    {
-        for (int j = 0; j < candidate_count; j++)
-        {
-            if (candidates[preferences[i][j]].eliminated == false)
-            {
-                candidates[preferences[i][j]].votes += 1;
-                break;
-            }
-        }
-
-    }
-
     return;
 }
 
@@ -178,52 +142,26 @@ void tabulate(void)
 bool print_winner(void)
 {
     // TODO
-    for (int i = 0; i < candidate_count; i++)
-    {
-        string most = candidates[i].name;
-        if (candidates[i].votes > voter_count / 2)
-        {
-            printf("%s\n", most);
-            return true;
-        }
-    }
     return false;
 }
 
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    int minvotes = voter_count;
-    for (int i = 0; i < candidate_count; i++)
-    {
-        if (candidates[i].eliminated == false && candidates[i].votes < minvotes)
-        {
-            minvotes = candidates[i].votes;
-        }
-    }
-    return minvotes;
+    // TODO
+    return 0;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
-bool is_tie(int minvotes)
+bool is_tie(int min)
 {
-    for (int i = 0; i < candidate_count; i++)
-    {
-        if (candidates[i].eliminated == false && candidates[i].votes != minvotes)
-        {
-            return false;
-        }
-    }
-    return true;
+    // TODO
+    return false;
 }
 
-// Eliminate the candidate (or candidiates) in last place
-void eliminate(int minvotes)
+// Eliminate the candidate (or candidates) in last place
+void eliminate(int min)
 {
-    for (int i = 0; i < candidate_count; i++)
-        if (candidates[i].votes == minvotes)
-        {
-            candidates[i].eliminated = true;
-        }
+    // TODO
     return;
 }
