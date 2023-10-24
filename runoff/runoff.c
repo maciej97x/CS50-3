a9e661616d0d21d7ba65fdbedf407a5a2fad3efd
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-24 08:29:17 +0000

    Tue, Oct 24, 2023, 8:29 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -143,6 +143,16 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     // TODO
+    for (int i = 0; i < voter_count; i++)
+    {
+        for(int j = 0; l < candidate_count; j++)
+        if(candidates[preferences[i][j]].eliminated == false)
+        {
+            candidates[preferences[i][j]].votes++;
+            break;
+
+        }
+    }
     return;
 }
 
