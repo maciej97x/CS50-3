dcd40e9ff3f21e7b584e3a235f0b7655cd693f1a
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-24 10:06:57 +0000

    Tue, Oct 24, 2023, 10:06 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -63,7 +63,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width / 2; j++)
         {
-            RGBTRIPLE temp = image[i][i];
+            RGBTRIPLE temp = image[i][j];
             image[i][j] = image[i][width - (j + 1)];
             image[i][width - (j + 1)] = temp;
         }
