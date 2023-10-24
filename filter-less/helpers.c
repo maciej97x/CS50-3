abe6fcaa8128769035aa17796287071a2c2aca62
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-24 09:11:28 +0000

    Tue, Oct 24, 2023, 9:11 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,21 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float Red = image[i][j].rgbtRed;
+            float Green = image[i][j].rgbtGreen;
+            float Blue = image[i][j].rgbtBlue;
+
+            int average = round((Red + Green + Blue) / 3);
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
+
+
+
+        }
+    }
     return;
 }
 
