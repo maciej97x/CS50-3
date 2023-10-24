6455def42d3010a60db90c4ced7d086b4315c8ae
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-24 08:36:42 +0000

    Tue, Oct 24, 2023, 8:36 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -160,6 +160,14 @@ void tabulate(void)
 bool print_winner(void)
 {
     // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > voter_count/2)
+        {
+            printf("%s\n", candidates[i].name);
+            return true;
+        }
+    }
     return false;
 }
 
