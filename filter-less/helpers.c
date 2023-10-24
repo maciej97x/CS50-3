7566f98d81c31ceb83f731d33698563cb2bdc3dc
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-24 09:29:25 +0000

    Tue, Oct 24, 2023, 9:29 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -22,9 +22,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height i++)
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width: j++)
+        for (int j = 0; j < width; j++)
         {
             float orRed = image[i][j].rgbtRed;
             float orGreen = image[i][j].rgbtGreen;
