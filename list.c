e1e2bf9c8dda3b4abbe4a99b4da6e027b5b10d56
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-25 17:06:56 +0000

    Wed, Oct 25, 2023, 5:06 PM +00:00

---
+++ list.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+int *list = malloc(3 * sizeof(int));
+if(list == NULL)
+{
+    return 1;
+}
+list[0] = 1;
+list[1] = 2;
+list[2] = 3;
+for (int i = 0; i < 3; i++)
+{
+    printf("%i\n", list[i]);
+}
+}
