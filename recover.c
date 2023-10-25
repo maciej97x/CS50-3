73b248ac83717809afb1d78b10f2f9ec25da8d1e
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-25 07:22:34 +0000

    Wed, Oct 25, 2023, 7:22 AM +00:00

--- recover.c
+++ recover.c
@@ -23,4 +23,9 @@ int main(int argc, char *argv[])
 
     FILE *output_file == NULL;
     char *filename == malloc(8* sizeof(char));
+
+    while (fread(buffer, sizeof(char), 512, input_file))
+    {
+        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+    }
 }
