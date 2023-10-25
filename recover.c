691f1904809d53e82f7e742294564a5657d92118
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-25 07:13:56 +0000

    Wed, Oct 25, 2023, 7:13 AM +00:00

--- recover.c
+++ recover.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <sdlib.h>
+#include <stdint.h>
+typedef uin8_t BYTE;
 
 int main(int argc, char *argv[])
 {
@@ -12,7 +14,13 @@ int main(int argc, char *argv[])
     FILE *input_file = fopen(argv[1], "r");
 
     if(input_file == NULL)
-
+    {
     printf("Could not open file");
     return 2;
+    }
+    unsigned char buffer[512];
+    int count_image = 0;
+
+    FILE *output_file == NULL;
+    char *filename == malloc(8* sizeof(char));
 }
