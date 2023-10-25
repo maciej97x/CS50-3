701705ca037107d7ea078aa68cb641d0fb453d61
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-25 07:38:15 +0000

    Wed, Oct 25, 2023, 7:38 AM +00:00

--- recover.c
+++ recover.c
@@ -25,25 +25,26 @@ int main(int argc, char *argv[])
     char *filename = malloc(8* sizeof(char));
 
     while (fread(buffer, sizeof(char), 512, input_file))
-    {
+
     {
         if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
-    
+
     {
         sprintf(filename, "%03i.jpg", count_image);
         output_file = fopen(filename, "w");
-        fount_image++;
+        count_image++;
 
     }
     if (output_file != NULL)
     {
-        fwrite(buffer, sizeof(char), 5122, output_file;)
+        fwrite(buffer, sizeof(char), 5122, output_file);
 
     }
-    }
+
     free(filename);
-    fclose(outpuf_file);
+    fclose(output_file);
     fclose(input_file);
-    return0;
+    return 0;
 
+}
 }
