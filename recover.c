b5a87448e625226613c1525bc80da6ed1dc7922b
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-25 07:07:44 +0000

    Wed, Oct 25, 2023, 7:07 AM +00:00

---
+++ recover.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
+    
+}
