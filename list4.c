3012d7b40c4d3b0e2e2d0b4c6114e9288b367e80
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-26 09:43:38 +0000

    Thu, Oct 26, 2023, 9:43 AM +00:00

---
+++ list4.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
+
+int main(int argc, char *argv[])
+{
+    node *list = NULL;
+
+    for (int i = 1; i < argc; i++)
+    {
+        int number = atoi(argv[i]);
+
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return 1;
+        }
+        n->number = number;
+        n->next = NULL;
+
+        n->next = list;
+        list = n;
+
+    }
+    node *ptr = list;
+    while (ptr != NULL)
+    {
+        printf("%i\n", ptr->number);
+        ptr = ptr->next;
+    }
+
+    ptr = list;
+    while (ptr != NULL)
+    {
+        node *next = ptr->next;
+        free(ptr);
+        ptr = next;
+
+    }
+}
