4c095a8af90d580e3cad2c13a7dd82e6f7bf590d
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-27 09:06:08 +0000

    Fri, Oct 27, 2023, 9:06 AM +00:00

--- inheritancee.c
+++ inheritancee.c
@@ -50,9 +50,14 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
+        p->parents[0] = parent0;
+        p->parents[1] = parent1;
 
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        p->alleles[0] = p->parents[0]->alleles[rand()%2];
+        p->alleles[1] = p->parents[1]->alleles[rand()%2];
+
 
 
 
@@ -87,7 +92,7 @@ void free_family(person *p)
 
 
     // TODO: Free child
-   
+
 }
 
 // Print each family member and their alleles.
