5be1e1b8bdcb95176a45c60151bb844ab0a52c78
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-27 08:59:01 +0000

    Fri, Oct 27, 2023, 8:59 AM +00:00

--- inheritancee.c
+++ inheritancee.c
@@ -50,12 +50,10 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        p->parents[0] = parent0;
-        p->parents[1] = parent1;
+
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        p->alleles[0] = p->parents[0]->alleles[rand()%2];
-        p->alleles[1] = p->parents[1]->alleles[rand()%2];
+
 
 
     }
@@ -64,12 +62,10 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-        p->parents[0] = NULL;
-        p->parents[1] = NULL;
+
 
         // TODO: Randomly assign alleles
-        p->alleles[0] = random_allele();
-        p->alleles[1] = random_allele();
+
 
 
     }
@@ -82,21 +78,16 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    if(p == NULL)
-    {
-        return;
-    }
+
+
 
 
     // TODO: Free parents recursively
-    free_family(p->parents[0]);
-    free_family(p->parents[0]);
 
 
 
     // TODO: Free child
-    free(p);
-
+   
 }
 
 // Print each family member and their alleles.
