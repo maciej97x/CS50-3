50acee2a8c086f1046394f7f2fa1bbc820e9e319
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-27 09:48:57 +0000

    Fri, Oct 27, 2023, 9:48 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -1,132 +1,195 @@
-// Implements a dictionary's functionality
+// Implements a spell-checker
 
 #include <ctype.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <strings.h>
+#include <sys/resource.h>
+#include <sys/time.h>
 
 #include "dictionary.h"
 
-// Represents a node in a hash table
-typedef struct node
+// Undefine any definitions
+#undef calculate
+#undef getrusage
+
+// Default dictionary
+#define DICTIONARY "dictionaries/large"
+
+// Prototype
+double calculate(const struct rusage *b, const struct rusage *a);
+
+int main(int argc, char *argv[])
 {
-    char word[LENGTH + 1];
-    struct node *next;
-}
-node;
-
-// TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
-
-// Hash table
-node *table[N];
-unsigned int word_count;
-unsigned int hash_value;
-
-// Returns true if word is in dictionary, else false
-bool check(const char *word)
-{
-    // TODO
-    hash_value = hash(word);
-    node *cursor = table[hash_value];
-
-    while (cursor != 0)
+    // Check for correct number of args
+    if (argc != 2 && argc != 3)
     {
-        if(strcasecmp(word, cursor->word) == 0)
-        {
-            return true;
-        }
-        cursor = cursor->next;
+        printf("Usage: ./speller [DICTIONARY] text\n");
+        return 1;
     }
-    return false;
-}
 
-// Hashes word to a number
-unsigned int hash(const char *word)
-{
-    // TODO: Improve this hash function
-    unsigned long total = 0;
+    // Structures for timing data
+    struct rusage before, after;
 
-    for (int i = 0; i < strlen(word); i++)
+    // Benchmarks
+    double time_load = 0.0, time_check = 0.0, time_size = 0.0, time_unload = 0.0;
+
+    // Determine dictionary to use
+    char *dictionary = (argc == 3) ? argv[1] : DICTIONARY;
+
+    // Load dictionary
+    getrusage(RUSAGE_SELF, &before);
+    bool loaded = load(dictionary);
+    getrusage(RUSAGE_SELF, &after);
+
+    // Exit if dictionary not loaded
+    if (!loaded)
     {
-        total += tolower(word[i]);
+        printf("Could not load %s.\n", dictionary);
+        return 1;
     }
-    
-    return total % N;
-}
 
-// Loads dictionary into memory, returning true if successful, else false
-bool load(const char *dictionary)
-{
-    // TODO
+    // Calculate time to load dictionary
+    time_load = calculate(&before, &after);
 
-}
-
-// Returns number of words in dictionary if loaded, else 0 if not yet loaded
-{
-    FILE *file = fopen(dicionary, "r");
-
+    // Try to open text
+    char *text = (argc == 3) ? argv[2] : argv[1];
+    FILE *file = fopen(text, "r");
     if (file == NULL)
     {
-    printf("Unable to open %s\n", dicionary);
-    return false;
-    }
-    char word[LENGHT+1];
-    while (fscanf(file, "%s", word) != E0F)
-    {
-    node *n + malloc(sizeof(node));
-
-    if (n == NULL)
-    {
-        return false;
+        printf("Could not open %s.\n", text);
+        unload();
+        return 1;
     }
 
-    strcpy(n->word,word);
-    hash_value = hash(word);
-    n->next = tabble[hash_value];
-    table[hash_value] = n;
-    word_count++;
+    // Prepare to report misspellings
+    printf("\nMISSPELLED WORDS\n\n");
+
+    // Prepare to spell-check
+    int index = 0, misspellings = 0, words = 0;
+    char word[LENGTH + 1];
+
+    // Spell-check each word in text
+    for (int c = fgetc(file); c != EOF; c = fgetc(file))
+    {
+        // Allow only alphabetical characters and apostrophes
+        if (isalpha(c) || (c == '\'' && index > 0))
+        {
+            // Append character to word
+            word[index] = c;
+            index++;
+
+            // Ignore alphabetical strings too long to be words
+            if (index > LENGTH)
+            {
+                // Consume remainder of alphabetical string
+                while ((c = fgetc(file)) != EOF && isalpha(c));
+
+                // Prepare for new word
+                index = 0;
+            }
+        }
+
+        // Ignore words with numbers (like MS Word can)
+        else if (isdigit(c))
+        {
+            // Consume remainder of alphanumeric string
+            while ((c = fgetc(file)) != EOF && isalnum(c));
+
+            // Prepare for new word
+            index = 0;
+        }
+
+        // We must have found a whole word
+        else if (index > 0)
+        {
+            // Terminate current word
+            word[index] = '\0';
+
+            // Update counter
+            words++;
+
+            // Check word's spelling
+            getrusage(RUSAGE_SELF, &before);
+            bool misspelled = !check(word);
+            getrusage(RUSAGE_SELF, &after);
+
+            // Update benchmark
+            time_check += calculate(&before, &after);
+
+            // Print word if misspelled
+            if (misspelled)
+            {
+                printf("%s\n", word);
+                misspellings++;
+            }
+
+            // Prepare for next word
+            index = 0;
+        }
     }
+
+    // Check whether there was an error
+    if (ferror(file))
+    {
+        fclose(file);
+        printf("Error reading %s.\n", text);
+        unload();
+        return 1;
+    }
+
+    // Close text
     fclose(file);
-    return true;
 
-}
+    // Determine dictionary's size
+    getrusage(RUSAGE_SELF, &before);
+    unsigned int n = size();
+    getrusage(RUSAGE_SELF, &after);
 
+    // Calculate time to determine dictionary's size
+    time_size = calculate(&before, &after);
 
-{
+    // Unload dictionary
+    getrusage(RUSAGE_SELF, &before);
+    bool unloaded = unload();
+    getrusage(RUSAGE_SELF, &after);
 
-}
-
-
-unsigned int size(void)
-{
-    // TODO
-    if(word_count > 0)
+    // Abort if dictionary not unloaded
+    if (!unloaded)
     {
-        return word_count;
+        printf("Could not unload %s.\n", dictionary);
+        return 1;
     }
+
+    // Calculate time to unload dictionary
+    time_unload = calculate(&before, &after);
+
+    // Report benchmarks
+    printf("\nWORDS MISSPELLED:     %d\n", misspellings);
+    printf("WORDS IN DICTIONARY:  %d\n", n);
+    printf("WORDS IN TEXT:        %d\n", words);
+    printf("TIME IN load:         %.2f\n", time_load);
+    printf("TIME IN check:        %.2f\n", time_check);
+    printf("TIME IN size:         %.2f\n", time_size);
+    printf("TIME IN unload:       %.2f\n", time_unload);
+    printf("TIME IN TOTAL:        %.2f\n\n",
+           time_load + time_check + time_size + time_unload);
+
+    // Success
     return 0;
 }
 
-// Unloads dictionary from memory, returning true if successful, else false
-bool unload(void)
+// Returns number of seconds between b and a
+double calculate(const struct rusage *b, const struct rusage *a)
 {
-    // TODO
-    for(int i = 0; i < N; i++)
+    if (b == NULL || a == NULL)
     {
-        node *cursor = table[i];
-        while(cursor)
-        {
-            node *tmp = cursor;
-            cursor = cursor->next;
-            free(tmp);
-        }
-        if (cursor == NULL)
-        {
-            return true;
-        }
+        return 0.0;
     }
-    return false;
+    else
+    {
+        return ((((a->ru_utime.tv_sec * 1000000 + a->ru_utime.tv_usec) -
+                  (b->ru_utime.tv_sec * 1000000 + b->ru_utime.tv_usec)) +
+                 ((a->ru_stime.tv_sec * 1000000 + a->ru_stime.tv_usec) -
+                  (b->ru_stime.tv_sec * 1000000 + b->ru_stime.tv_usec)))
+                / 1000000.0);
+    }
 }
